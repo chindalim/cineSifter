74b1f8f33936c9c86d77f4d02686c7e9ebd4bd16
Chendar Lim <chendar.lim@live.com>
2020-11-20 14:20:42 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = "3ff9aa5f";
+  const APP_KEY = "a1f2e3c4ac058cd771485456580ba360	";
+
+  useEffect(() => {
+    //getMovies();
+    }, []
+  );
+
+  const getCine = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = response.json();
+    console.log(data);
+
+    //this is the promise - fetch & then
+
+  }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cine Sifter</h1>
+
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+
     </div>
   );
 }
